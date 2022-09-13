f2080e06a0469579b744b093c773254388ef5d9c
Анастасия <195298@gmail.com>
2022-09-13 18:05:37 +0300

    build not found

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import csso from 'postcss-csso';
+import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import  htmlmin from 'gulp-htmlmin';
 
 // Styles
 
@@ -16,8 +18,16 @@ export const styles = () => {
       autoprefixer(),
       csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe (rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+//HTML
+export const htmlmin = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'));
 }
 
 // Server
